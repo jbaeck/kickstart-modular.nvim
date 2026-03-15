c0a836b1049eb259d877cc0895eb27c517ee3800
Joachim Baeck <joachim.baeck@rewe-group.com>
2026-03-15 15:57:05 +0100

    Added onenord theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -35,7 +35,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.blink-cmp',
 
-  require 'kickstart.plugins.tokyonight',
+  -- require 'kickstart.plugins.tokyonight',
 
   require 'kickstart.plugins.todo-comments',
 
@@ -44,6 +44,8 @@ require('lazy').setup({
   require 'kickstart.plugins.treesitter',
 
   require 'custom.plugins.lualine',
+
+  require 'kickstart.plugins.onenord',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
